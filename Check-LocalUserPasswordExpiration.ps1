46da470dc4f4d82112d692e87259d450627e4870
Cysadmin <tim.matzenauer@outlook.de>
2024-04-18 20:15:41 +0200

    Udpated

--- Check-LocalUserPasswordExpiration.ps1
+++ Check-LocalUserPasswordExpiration.ps1
@@ -3,3 +3,8 @@ $fullUserName = (Get-CimInstance -Class   Win32_ComputerSystem | Select-Object U
 $currentUserName = $fullUserName.split("\")[1]
 $currentUser = Get-LocalUser -Name $currentUserName
 $passwordExpiration = $currentUser.PasswordExpires   
+
+
+# FOR PS2EXE WITH CONSOLE OPUTPUT
+# Write-Host -NoNewLine 'Press any key to continue...';
+#$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
